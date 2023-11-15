300bd6f24f2c7b85cb1f4319b4a913fae9cd6cde
21127334 <lvlam21@clc.fitus.edu.vn>
2023-11-16 01:08:04 +0700

    done

--- src/main/java/StudentManagement/StudentManagementApp.java
+++ src/main/java/StudentManagement/StudentManagementApp.java
@@ -2,67 +2,131 @@
  * Author: lamlevungan
  * Created date: Wed 15/11/2023
  * Package: StudentManagement
- * File name: StudentManagement.java
+ * File name: StudentManagementApp.java
  */
 
 package StudentManagement;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
 
-public class StudentManagement {
+public class StudentManagementApp {
+    private static StudentManagementCore db;
+    private static JList<String> studentsJList;
 
-    private static void importFile(Container contentPane){
-        JPanel importPanel = new JPanel(new FlowLayout());
-
-        JLabel title = new JLabel("Enter data file name:");
-        JTextField filePathField = new JTextField(16); // width = 16 cols
-        JButton importButton = new JButton("Import");
-
-        importPanel.add(title);
-        importPanel.add(filePathField);
-        importPanel.add(importButton);
-
-        importPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        contentPane.add(importPanel);
-    }
-
-    private static void studentsList(Container contentPane){
+    private static void studentsList(Container contentPane) {
         // TITLE
+        JPanel titleContainer = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
+
         JLabel studentTitle = new JLabel("Students List");
         studentTitle.setHorizontalAlignment(JLabel.CENTER);
         studentTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        contentPane.add(studentTitle);
+        JButton refresh = new JButton("Refresh");
+
+        JLabel helper = new JLabel("Click to a student to see full information");
+        helper.setHorizontalAlignment(JLabel.CENTER);
+        helper.setAlignmentX(Component.CENTER_ALIGNMENT);
+        refresh.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    List<String> rs = db.getListIdAndName();
+                    studentsJList.setListData(rs.toArray(new String[0]));
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+
+        titleContainer.add(studentTitle);
+        titleContainer.add(refresh);
+
+        contentPane.add(titleContainer);
+        contentPane.add(helper);
         contentPane.add(Box.createVerticalStrut(10));
 
         // LIST
-        String[] students = {"Student 1", "Student 2", "Student 3", "Student 4", "Student 5", "Student 6"};
+        String[] students = {};
 
-        JList<String> studentsJList = new JList<>(students);
+        studentsJList = new JList<>(students);
+        studentsJList.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (!e.getValueIsAdjusting()) {
+                    // Single-click detected
+                    int selectedIndex = studentsJList.getSelectedIndex();
+                    if (selectedIndex != -1) {
+                        String selectedStudent = studentsJList.getModel().getElementAt(selectedIndex);
+                        try {
+                            showFullInformation(contentPane, selectedStudent);
+                        } catch (SQLException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    }
+                }
+            }
+        });
         JScrollPane scrollPane = new JScrollPane(studentsJList);
 
         // Set some properties for the list
         studentsJList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
-
         studentsJList.setAlignmentX(Component.CENTER_ALIGNMENT);
         contentPane.add(scrollPane, BorderLayout.CENTER);
     }
 
-    private static void operationsList(Container contentPane){
+    private static void operationsList(Container contentPane) {
         JPanel buttonContainer = new JPanel();
         buttonContainer.setLayout(new BoxLayout(buttonContainer, BoxLayout.Y_AXIS));
-        buttonContainer.setBorder(new EmptyBorder(16,16,16,16));
+        buttonContainer.setBorder(new EmptyBorder(16, 16, 16, 16));
 
-        JLabel title = new JLabel("List operations:");
+        JLabel title = new JLabel("List Operations:");
 
         JButton addStudent = new JButton("Add New Student");
+        addStudent.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showAddDialog(contentPane);
+            }
+        });
+
         JButton updateStudent = new JButton("Update Student");
+        updateStudent.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showUpdateDialog(contentPane);
+            }
+        });
         JButton removeStudent = new JButton("Remove Student");
+        removeStudent.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showDeleteDialog(contentPane);
+            }
+        });
         JButton importList = new JButton("Import List Student");
+        importList.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showImportDialog(contentPane);
+            }
+        });
         JButton exportList = new JButton("Export List Student");
+        exportList.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showExportDialog(contentPane);
+            }
+        });
 
         buttonContainer.add(title);
         buttonContainer.add(Box.createVerticalStrut(10)); // Space between 2 components
@@ -82,6 +146,325 @@ public class StudentManagement {
         contentPane.add(buttonContainer, BorderLayout.WEST);
     }
 
+    private static void showFullInformation(Container contentPane, String info) throws SQLException {
+        String infoId = info.split(" - ")[0];
+        List<String> rs = db.getFullInformation(Integer.parseInt(infoId));
+        JDialog showDeleteDialog = new JDialog((JFrame) SwingUtilities.getWindowAncestor(contentPane), "Full Information Of  "+infoId, true);
+        showDeleteDialog.setLayout(new BorderLayout());
+
+        JPanel dialogPanel = new JPanel();
+        dialogPanel.setLayout(new BoxLayout(dialogPanel, BoxLayout.Y_AXIS));
+        dialogPanel.setBorder(new EmptyBorder(16, 16, 16, 16));
+
+        // Add your components to the dialog
+        for (String data : rs){
+            JLabel singleData = new JLabel(data);
+            dialogPanel.add(singleData);
+        }
+
+        // Show dialog
+        showDeleteDialog.add(dialogPanel);
+
+        showDeleteDialog.setLocationRelativeTo(contentPane);
+        showDeleteDialog.pack();
+        showDeleteDialog.setVisible(true);
+
+    }
+
+    private static void showAddDialog(Container contentPane) {
+        JDialog showAddDialog = new JDialog((JFrame) SwingUtilities.getWindowAncestor(contentPane), "Add Data", true);
+        showAddDialog.setLayout(new BorderLayout());
+
+        // Add your components to the dialog
+        JLabel id = new JLabel("Id:");
+        JTextField idField = new JTextField(16);
+        JLabel name = new JLabel("Name:");
+        JTextField nameField = new JTextField(16);
+        JLabel gpa = new JLabel("GPA:");
+        JTextField gpaField = new JTextField(16);
+        JLabel image = new JLabel("Image:");
+        JTextField imageField = new JTextField(16);
+        JLabel address = new JLabel("Address:");
+        JTextField addressField = new JTextField(16);
+        JLabel notes = new JLabel("Notes:");
+        JTextField notesField = new JTextField(16);
+
+        JButton addButton = new JButton("Add");
+
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int id = Integer.parseInt(idField.getText());
+                String name = nameField.getText();
+                float gpa = Float.parseFloat(gpaField.getText());
+                String image = imageField.getText();
+                String address = addressField.getText();
+                String notes = notesField.getText();
+
+                try {
+                    db.insertData(id, name, gpa, image, address, notes);
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+
+                showAddDialog.dispose();
+            }
+        });
+
+        // Add components to inside dialog
+
+        JPanel dialogPanel = new JPanel();
+        dialogPanel.setLayout(new BoxLayout(dialogPanel, BoxLayout.Y_AXIS));
+        dialogPanel.setBorder(new EmptyBorder(16, 16, 16, 16));
+
+        dialogPanel.add(id);
+        dialogPanel.add(idField);
+        dialogPanel.add(name);
+        dialogPanel.add(nameField);
+        dialogPanel.add(gpa);
+        dialogPanel.add(gpaField);
+        dialogPanel.add(image);
+        dialogPanel.add(imageField);
+        dialogPanel.add(address);
+        dialogPanel.add(addressField);
+        dialogPanel.add(notes);
+        dialogPanel.add(notesField);
+
+        dialogPanel.add(addButton);
+
+        // Show dialog
+        showAddDialog.add(dialogPanel, BorderLayout.CENTER);
+
+        showAddDialog.setLocationRelativeTo(contentPane);
+        showAddDialog.pack();
+        showAddDialog.setVisible(true);
+    }
+    private static void showUpdateDialog(Container contentPane) {
+        JDialog showUpdateDialog = new JDialog((JFrame) SwingUtilities.getWindowAncestor(contentPane), "Update Data", true);
+        showUpdateDialog.setLayout(new BorderLayout());
+
+        // Add your components to the dialog
+        JLabel id = new JLabel("Id:");
+        JTextField idField = new JTextField(16);
+        JLabel name = new JLabel("Name:");
+        JTextField nameField = new JTextField(16);
+        JLabel gpa = new JLabel("GPA:");
+        JTextField gpaField = new JTextField(16);
+        JLabel image = new JLabel("Image:");
+        JTextField imageField = new JTextField(16);
+        JLabel address = new JLabel("Address:");
+        JTextField addressField = new JTextField(16);
+        JLabel notes = new JLabel("Notes:");
+        JTextField notesField = new JTextField(16);
+
+        JButton updateButton = new JButton("Edit");
+
+        updateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int id = Integer.parseInt(idField.getText());
+                String name = nameField.getText();
+                float gpa = Float.parseFloat(gpaField.getText());
+                String image = imageField.getText();
+                String address = addressField.getText();
+                String notes = notesField.getText();
+
+                try {
+                    db.editData(id, name, gpa, image, address, notes);
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+                showUpdateDialog.dispose();
+            }
+        });
+
+        // Add components to inside dialog
+
+        JPanel dialogPanel = new JPanel();
+        dialogPanel.setLayout(new BoxLayout(dialogPanel, BoxLayout.Y_AXIS));
+        dialogPanel.setBorder(new EmptyBorder(16, 16, 16, 16));
+
+        dialogPanel.add(id);
+        dialogPanel.add(idField);
+        dialogPanel.add(name);
+        dialogPanel.add(nameField);
+        dialogPanel.add(gpa);
+        dialogPanel.add(gpaField);
+        dialogPanel.add(image);
+        dialogPanel.add(imageField);
+        dialogPanel.add(address);
+        dialogPanel.add(addressField);
+        dialogPanel.add(notes);
+        dialogPanel.add(notesField);
+
+        dialogPanel.add(updateButton);
+
+        // Show dialog
+        showUpdateDialog.add(dialogPanel, BorderLayout.CENTER);
+
+        showUpdateDialog.setLocationRelativeTo(contentPane);
+        showUpdateDialog.pack();
+        showUpdateDialog.setVisible(true);
+    }
+
+    private static void showDeleteDialog(Container contentPane) {
+        JDialog showDeleteDialog = new JDialog((JFrame) SwingUtilities.getWindowAncestor(contentPane), "Delete Data", true);
+        showDeleteDialog.setLayout(new BorderLayout());
+
+        // Add your components to the dialog
+        JLabel id = new JLabel("Id:");
+        JTextField idField = new JTextField(16);
+
+        JButton addButton = new JButton("Delete");
+
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int id = Integer.parseInt(idField.getText());
+
+                try {
+                    db.deleteData(id);
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+                showDeleteDialog.dispose();
+            }
+        });
+
+        // Add components to inside dialog
+
+        JPanel dialogPanel = new JPanel();
+        dialogPanel.setLayout(new FlowLayout());
+        dialogPanel.setBorder(new EmptyBorder(16, 16, 16, 16));
+
+        dialogPanel.add(id);
+        dialogPanel.add(idField);
+
+        dialogPanel.add(addButton);
+
+        // Show dialog
+        showDeleteDialog.add(dialogPanel);
+
+        showDeleteDialog.setLocationRelativeTo(contentPane);
+        showDeleteDialog.pack();
+        showDeleteDialog.setVisible(true);
+    }
+
+    private static void showImportDialog(Container contentPane) {
+        JDialog importDialog = new JDialog((JFrame) SwingUtilities.getWindowAncestor(contentPane), "Import Data", true);
+        importDialog.setLayout(new BorderLayout());
+
+        // Add your components to the dialog
+        JButton closeButton = new JButton("Close");
+        closeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                importDialog.dispose();
+            }
+        });
+
+        JLabel title = new JLabel("Enter data file name:");
+        JTextField filePathField = new JTextField(16); // width = 16 cols
+
+        JButton importButton = new JButton("Import");
+
+        importButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String fileName = filePathField.getText();
+
+                try {
+                    db.importData(fileName);
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+
+                importDialog.dispose();
+            }
+        });
+
+        // Add components to inside dialog
+
+        JPanel dialogPanel = new JPanel();
+        dialogPanel.setLayout(new BoxLayout(dialogPanel, BoxLayout.Y_AXIS));
+        dialogPanel.setBorder(new EmptyBorder(16, 16, 16, 16));
+
+        dialogPanel.add(title);
+        dialogPanel.add(filePathField);
+        dialogPanel.add(importButton);
+        dialogPanel.add(closeButton);
+
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        filePathField.setAlignmentX(Component.CENTER_ALIGNMENT);
+        importButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        closeButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Show dialog
+        importDialog.add(dialogPanel, BorderLayout.CENTER);
+
+        importDialog.setLocationRelativeTo(contentPane);
+        importDialog.pack();
+        importDialog.setVisible(true);
+    }
+
+    private static void showExportDialog(Container contentPane) {
+        JDialog exportDialog = new JDialog((JFrame) SwingUtilities.getWindowAncestor(contentPane), "Export Data", true);
+        exportDialog.setLayout(new BorderLayout());
+
+        // Add your components to the dialog
+        JButton closeButton = new JButton("Close");
+        closeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                exportDialog.dispose();
+            }
+        });
+
+        JLabel title = new JLabel("Enter file name you want to export data to:");
+        JTextField filePathField = new JTextField(16); // width = 16 cols
+        JButton exportButton = new JButton("Export");
+        exportButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String fileName = filePathField.getText();
+
+                try {
+                    db.exportData(fileName);
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+                exportDialog.dispose();
+            }
+        });
+        // Add components to inside dialog
+
+        JPanel dialogPanel = new JPanel();
+        dialogPanel.setLayout(new BoxLayout(dialogPanel, BoxLayout.Y_AXIS));
+        dialogPanel.setBorder(new EmptyBorder(16, 16, 16, 16));
+
+        dialogPanel.add(title);
+        dialogPanel.add(filePathField);
+        dialogPanel.add(exportButton);
+        dialogPanel.add(closeButton);
+
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        filePathField.setAlignmentX(Component.CENTER_ALIGNMENT);
+        exportButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        closeButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Show dialog
+        exportDialog.add(dialogPanel, BorderLayout.CENTER);
+
+        exportDialog.setLocationRelativeTo(contentPane);
+        exportDialog.pack();
+        exportDialog.setVisible(true);
+    }
+
     private static void addComponents(Container contentPane) {
         contentPane.setLayout(new BorderLayout());
 
@@ -98,10 +481,10 @@ public class StudentManagement {
         // CENTER
         JPanel centerPanel = new JPanel();
         centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
-        centerPanel.setBorder(new EmptyBorder(16,16,16,16));
+        centerPanel.setBorder(new EmptyBorder(16, 16, 16, 16));
         // Default size
-        Dimension preferredSize = new Dimension(300, centerPanel.getPreferredSize().height);
-        centerPanel.setPreferredSize(preferredSize);
+//        Dimension preferredSize = new Dimension(300, centerPanel.getPreferredSize().height);
+//        centerPanel.setPreferredSize(preferredSize);
 
         studentsList(centerPanel);
 
@@ -136,11 +519,13 @@ public class StudentManagement {
         mainFrame.setVisible(true);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        db = new StudentManagementCore();
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndShowGUI();
             }
         });
+
     }
 }
--- src/main/java/StudentManagement/StudentManagementCore.java
+++ src/main/java/StudentManagement/StudentManagementCore.java
@@ -1,12 +1,168 @@
 /**
  * Author: lamlevungan
- * Created date: Wed 15/11/2023 
+ * Created date: Wed 15/11/2023
  * Package: StudentManagement
  * File name: StudentManagementCore.java
  */
- 
+
 package StudentManagement;
 
-public class StudentManagementCore{
-    
+import java.io.*;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentManagementCore {
+    private static Connection con = null;
+    private static String databaseName = "StudentDatabase";
+    private static String url = "jdbc:mysql://localhost:3306/" + databaseName;
+
+    private static String username = "root";
+    private static String password = "12345678";
+
+    StudentManagementCore() throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+        Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+        con = DriverManager.getConnection(url, username, password);
+    }
+
+    public static void closeConnection() throws SQLException {
+        con.close();
+    }
+
+    public static void importData(String fileName) throws SQLException {
+        Statement st = con.createStatement();
+        try (
+                BufferedReader in = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            line = in.readLine(); // Header line
+            while ((line = in.readLine()) != null) {
+                String[] infoFields = line.split(",");
+                // id, name, gpa, image, address, notes
+                if (infoFields.length == 6) {
+                    int id = Integer.parseInt(infoFields[0]);
+                    String name = infoFields[1];
+                    double gpa = Double.parseDouble(infoFields[2]);
+                    String image = infoFields[3];
+                    String address = infoFields[4];
+                    String notes = infoFields[5];
+                    insertData(id, name, gpa, image, address, notes);
+                }
+            }
+            in.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void insertData(int id, String name, double gpa, String image, String address, String notes) throws SQLException {
+        String insertQuery = "INSERT INTO Student (id, name, gpa, image, address, notes) VALUES (?, ?, ?, ?, ?, ?)";
+
+        try (PreparedStatement preparedStatement = con.prepareStatement(insertQuery)) {
+            preparedStatement.setInt(1, id);
+            preparedStatement.setString(2, name);
+            preparedStatement.setDouble(3, gpa);
+            preparedStatement.setString(4, image);
+            preparedStatement.setString(5, address);
+            preparedStatement.setString(6, notes);
+
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public static void editData(int id, String name, double gpa, String image, String address, String notes) throws SQLException {
+        String insertQuery = "UPDATE Student SET name = ?, gpa = ?, image = ?, address = ?, notes = ? where id = ?;";
+
+        try (PreparedStatement preparedStatement = con.prepareStatement(insertQuery)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setDouble(2, gpa);
+            preparedStatement.setString(3, image);
+            preparedStatement.setString(4, address);
+            preparedStatement.setString(5, notes);
+            preparedStatement.setInt(6, id);
+
+            preparedStatement.executeUpdate();
+        }
+    }
+    public static void deleteData(int id) throws SQLException {
+        String deleteQuery = "DELETE FROM Student WHERE id = ?;";
+
+        try (PreparedStatement preparedStatement = con.prepareStatement(deleteQuery)) {
+            preparedStatement.setInt(1, id);
+
+            preparedStatement.executeUpdate();
+        }
+    }
+    public static List<String> getListIdAndName() throws SQLException {
+        List<String> idAndNameList = new ArrayList<>();
+        Statement st = con.createStatement();
+        String query = "select id, name from Student";
+
+        ResultSet rs = st.executeQuery(query);
+
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+
+            String idAndName = id + " - " + name;
+            idAndNameList.add(idAndName);
+        }
+        return idAndNameList;
+    }
+
+    public static List<String> getFullInformation(int infoId) throws SQLException{
+        List<String> student = new ArrayList<>();
+
+        String query = "select * from Student where id = ?";
+
+        try (PreparedStatement pst = con.prepareStatement(query)) {
+            pst.setInt(1, infoId);
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                double gpa = rs.getDouble("gpa");
+                String image = rs.getString("image");
+                String address = rs.getString("address");
+                String notes = rs.getString("notes");
+
+                student.add("Id: "+id);
+                student.add("Name: "+name);
+                student.add("GPA: "+gpa);
+                student.add("Image: "+image);
+                student.add("Address: "+address);
+                student.add("Notes: "+notes);
+            }
+        }
+
+        return student;
+    }
+
+    public static void exportData(String fileName) throws SQLException {
+        Statement st = con.createStatement();
+        String query = "select * from Student";
+
+        ResultSet rs = st.executeQuery(query);
+
+        try (
+                BufferedWriter out = new BufferedWriter(new FileWriter(fileName))) {
+            // Header line: id, name, gpa, image, address, notes
+            out.write("id,name,gpa,image,address,notes\n");
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                float gpa = rs.getFloat("gpa");
+                String image = rs.getString("image");
+                String address = rs.getString("address");
+                String notes = rs.getString("notes");
+
+                out.write(id + "," + name + "," + gpa + ","
+                        + image + "," + address + "," + notes + "\n");
+            }
+            out.close();
+            System.out.println("Export successfully to " + fileName);
+        } catch (IOException e) {
+            System.out.println("ERROR: " + e);
+        }
+    }
 }
